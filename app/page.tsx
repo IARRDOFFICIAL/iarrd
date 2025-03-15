f519e848c1d5d4e1c0645fb69d6b3844ee4ba6dc
IARRD <iarrd.official@gmail.com>
2025-03-15 21:35:38 +0530

    have updated background for final

--- app/page.tsx
+++ app/page.tsx
@@ -205,7 +205,7 @@ export default function Home() {
           <div 
             className="absolute inset-0 bg-cover bg-center bg-no-repeat"
             style={{
-              backgroundImage: "url('https://images.unsplash.com/photo-1457364887197-9150188c107b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+              backgroundImage: "url('https://i.pinimg.com/736x/af/a4/e9/afa4e9e4c9e0cfd2f500fb36067571d3.jpg')",
             }}
           >
             <div className="absolute inset-0 bg-black/50" />
