53c782e41bfe67ce85d7be3df3d936fb1e34e2ec
IARRD <iarrd.official@gmail.com>
2025-06-20 12:34:04 +0530

    updated the hypen

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
     {
       name: "Goutham GV",
       role: "CFO & Fund & Finance Lead",
-      bio: ""Goutham GV serves as the Chief Financial Officer at IARRD, managing all financial strategies and fundraising initiatives. With a keen focus on budgeting, resource allocation, and funding operations, he ensures financial stability while supporting innovation and sustainable growth.",
+      bio: "Goutham GV serves as the Chief Financial Officer at IARRD, managing all financial strategies and fundraising initiatives. With a keen focus on budgeting, resource allocation, and funding operations, he ensures financial stability while supporting innovation and sustainable growth.",
       photo: "https://in.pinterest.com/pin/987343918314864009/"
     },
     {
